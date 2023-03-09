f238204a63e26dda0c6c656a379e8a3891141463
RashiniNuwangi <rnuwangi3@gmail.com>
2023-03-09 22:38:43 +0530

    2023/09/03

---
+++ TECMIS - 1/src/com/tecmis/B05/notice/Notice.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tecmis.B05.notice;
+
+/**
+ *
+ * @author Vivobook
+ */
+public class Notice {
+    
+    private String NoticeID;
+    private String Title;
+    private String Date_Time;
+    private String FilePath;
+    private String Description;
+
+    public String getNoticeID() {
+        return NoticeID;
+    }
+
+    public void setNoticeID(String NoticeID) {
+        this.NoticeID = NoticeID;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public String getDate_Time() {
+        return Date_Time;
+    }
+
+    public void setDate_Time(String Date_Time) {
+        this.Date_Time = Date_Time;
+    }
+
+    public String getFilePath() {
+        return FilePath;
+    }
+
+    public void setFilePath(String FilePath) {
+        this.FilePath = FilePath;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+    
+    
+}
