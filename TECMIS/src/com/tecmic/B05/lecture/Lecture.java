024769a5d45e31331bffdbc919cdf6ab41f61845
BANDARAKDT <deshanibandarawas@gmail.com>
2023-03-09 22:56:56 +0530

    code lecturer methods
    coding basics methods of lecturer class

--- TECMIS/src/com/tecmic/B05/lecture/Lecture.java
+++ TECMIS/src/com/tecmic/B05/lecture/Lecture.java
@@ -4,10 +4,57 @@
  */
 package com.tecmic.B05.lecture;
 
+import com.tecmic.B05.user.User;
+
 /**
  *
  * @author Vivobook
  */
-public class Lecture {
+public class Lecture extends User{
     
-}
+    
+    
+   public void updateDetails(){
+
+} 
+   public void AddMatirial(){
+       
+   }
+   
+   public void DeleteMatirial(){
+       
+   }
+   
+   public void editMatirial(){
+       
+   }
+   
+   public void uploadMarks(){
+       
+   }
+   
+   public void veiwStudentDetails(){
+       
+   }
+   
+   public void veiwEligibility(){
+       
+   }
+    
+        
+        
+   public void seeMarkSheet(){
+       
+   }
+   
+   public void veiwAttendence(){
+       
+   }
+   
+   public void veiwmedical(){
+       
+   }
+   
+    }
+
+
