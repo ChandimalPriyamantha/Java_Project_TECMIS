cc423f79d237708c3c1a80ef5891077273c3c799
RashiniNuwangi <rnuwangi3@gmail.com>
2023-03-09 22:29:19 +0530

    3/9/2023 Methods

--- TECMIS/src/com/tecmic/B05/Login/LoginForm.java
+++ TECMIS/src/com/tecmic/B05/Login/LoginForm.java
@@ -13,3 +13,5 @@ public abstract class LoginForm{
     abstract void Login();
     
 }
+
+
--- TECMIS/src/com/tecmis/BO5/student/Student.java
+++ TECMIS/src/com/tecmis/BO5/student/Student.java
@@ -4,10 +4,36 @@
  */
 package com.tecmis.BO5.student;
 
+import com.tecmic.B05.user.User;
+
 /**
  *
  * @author Vivobook
  */
-public class Student {
+public class Student extends User
+{
+    public void updateDetails()
+    {
+        
+    }
     
+    public void viewMedical()
+    {
+        
+    }
+    
+    public void viewAttendece()
+    {
+        
+    }
+    
+    public  void CourseDeatils()
+    {
+        
+    }
+    
+    public void viewResult()
+    {
+        
+    }
 }
